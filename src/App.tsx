64b788d74212e2ce14de71d71462618c0fff6117
gogden245 <130518421+gogden245@users.noreply.github.com>
2024-03-08 19:28:08 -0700

    Finished Videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface BandProps {
+  name: string;
+  members: string;
+  formed: number;
+}
+
+const bandNames = [
+  {
+    name: 'The Beastie Boys',
+    members: 'Ad Rock, MCA, Mike D',
+    formed: 1979,
+  },
+  {
+    name: 'Wolfmother',
+    members: 'Andrew Stockdale, Hamish Rosser, Bobby Poulton',
+    formed: 2004,
+  },
+  {
+    name: 'Cream',
+    members: 'Eric Clapton, Jack Bruce, Ginger Baker',
+    formed: 1966,
+  },
+  {
+    name: 'Nirvana',
+    members: 'Kurt Cobain, Dave Groul, Krist Novoselic',
+    formed: 1987,
+  },
+];
+
+function Welcome() {
+  return <h1>The Best Music Trios Ever</h1>;
+}
+
+class Band extends React.Component<BandProps> {
+  // This is a component- similar to a function but different
+  render() {
+    const oneBand = this.props;
+
+    return (
+      <div>
+        <img></img>
+        <h2>{oneBand.name}</h2>
+        <h3>Members: {oneBand.members}</h3>
+        <h3>Formed: {oneBand.formed}</h3>
+      </div>
+    );
+  }
+}
+
+function BandList() {
+  // this is a function
   return (
+    // These components need to be wrapped in a div tag; it has to be just one thing being returned
+    <div>
+      {/* <Band {...bandNames[0]} />
+      <Band {...bandNames[1]} />
+      <Band {...bandNames[2]} /> 
+      
+      Instead of doing it this way ^ (it gets harder as data increases), do it this way below: */}
+
+      {bandNames.map((bandNum) => (
+        <Band {...bandNum} />
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  // default main view
+
+  return (
+    // Here we call that function
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <BandList />
     </div>
   );
 }
